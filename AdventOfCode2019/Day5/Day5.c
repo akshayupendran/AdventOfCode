e62aa4c19f1f1be445593f654159f0f4f114430c
akshayupendran <akshaykrishna.upendran@gmail.com>
2019-12-13 14:52:13 +0530

    Day 5 Finalised

--- AdventOfCode2019/Day5/Day5.c
+++ AdventOfCode2019/Day5/Day5.c
@@ -4,64 +4,85 @@
 
 /* INTOCDE COMPUTER DEFINES */
 
-#define NUMBER_OF_INPUTS 678u
+#define NUMBER_OF_INPUTS 678
 
 long long int input[NUMBER_OF_INPUTS];
 FILE *fp;
 long long int IntcodeProgram[NUMBER_OF_INPUTS];
-
+int Parameter1Mode=0, Parameter2Mode=0, Parameter3Mode=0, Opcode, ProgramCounter=0;
+long long int *Parameter1, *Parameter2, *Parameter3;
+    
 void GetInputs();
 void PrintInputs();
 void RunIntCodeComputer();
+void AssignParameters();
 
+void AssignParameters()
+{
+    Opcode = (int)((IntcodeProgram[ProgramCounter])%100);
+    if(99 != Opcode)
+    {
+        /* Paramter 1*/
+        Parameter1Mode = (int)(((IntcodeProgram[ProgramCounter])/100)%10);
+        if(1 < Parameter1Mode)
+        {
+            printf("Shit Happens in Parameter Mode1\n");
+        }
+        if(Parameter1Mode == 0)
+        {
+            Parameter1= &(IntcodeProgram[IntcodeProgram[ProgramCounter+1]]);
+        }
+        else
+        {
+            Parameter1= &(IntcodeProgram[ProgramCounter+1]);
+        }
+            
+            
+        if((3!=Opcode) && (4!=Opcode))
+        {
+            /* Paramter 2*/
+            Parameter2Mode = (int)(((IntcodeProgram[ProgramCounter])/1000)%10);
+            if(1 < Parameter2Mode)
+            {
+                printf("Shit Happens in Parameter Mode1\n");
+            }
+            if(Parameter2Mode == 0)
+            {
+                Parameter2= &(IntcodeProgram[IntcodeProgram[ProgramCounter+2]]);
+            }
+            else
+            {
+                Parameter2= &(IntcodeProgram[ProgramCounter+2]);
+            }
+                
+            
+            if((5!=Opcode) && (6!=Opcode))
+            {
+                /* Paramter 3*/
+                Parameter3Mode = (int)(((IntcodeProgram[ProgramCounter])/10000)%10);
+                if(1 < Parameter3Mode)
+                {
+                    printf("Shit Happens in Parameter Mode1\n");
+                }
+                if(Parameter3Mode == 0)
+                {
+                    Parameter3= &(IntcodeProgram[IntcodeProgram[ProgramCounter+3]]);
+                }
+                else
+                {
+                    Parameter3= &(IntcodeProgram[ProgramCounter+3]);
+                }
+            }
+        }
+    }
+}
 void RunIntCodeComputer()
 {
-	int ProgramCounter=0;
     /* Sanity Check before running Code */
-	int Parameter1Mode, Parameter2Mode, Parameter3Mode, Opcode;
-	long long int *Parameter1, *Parameter2, *Parameter3;
-	
     for(ProgramCounter=0; ProgramCounter<NUMBER_OF_INPUTS;)
     {
-		Opcode = (int)((IntcodeProgram[ProgramCounter])%100);
-		Parameter1Mode = (int)(((IntcodeProgram[ProgramCounter])/100)%10);
-		Parameter2Mode = (int)(((IntcodeProgram[ProgramCounter])/1000)%10);
-		Parameter3Mode = (int)(((IntcodeProgram[ProgramCounter])/10000)%10);
-		
-		/* Sanity Check of Parameter Modes */
-		if((1 < Parameter1Mode) || (1 < Parameter2Mode) || (1 < Parameter3Mode))
-		{
-			printf("Shit Happens in Parameter Modes");
-		}
-		
-		/* Get Parameters as pointers */
-		if(Parameter1Mode == 0)
-		{
-			Parameter1= &(IntcodeProgram[IntcodeProgram[ProgramCounter+1]]);
-		}
-		else
-		{
-			Parameter1= &(IntcodeProgram[ProgramCounter+1]);
-		}
-		if(Parameter2Mode == 0)
-		{
-			Parameter2= &(IntcodeProgram[IntcodeProgram[ProgramCounter+2]]);
-		}
-		else
-		{
-			Parameter1= &(IntcodeProgram[ProgramCounter+2]);
-		}
-		if(Parameter3Mode == 0)
-		{
-			Parameter3= &(IntcodeProgram[IntcodeProgram[ProgramCounter+3]]);
-		}
-		else
-		{
-			Parameter1= &(IntcodeProgram[ProgramCounter+3]);
-		}
-		
-		printf("Current Opcode, Parameters Modes and Parameters are: %d %d %d %d %lld %lld %lld\n", Opcode, Parameter1Mode, Parameter2Mode, Parameter3Mode, Parameter1[0], Parameter2[0], Parameter3[0]);
-		/* Start the INTCODE COMPUTER */
+		AssignParameters();
+        /* Start the INTCODE COMPUTER */
         if(99 == Opcode)
         {
             ProgramCounter=NUMBER_OF_INPUTS;
@@ -88,10 +109,59 @@ void RunIntCodeComputer()
 			printf("%lld\n",Parameter1[0]);
 			ProgramCounter+=2;
 		}
+        else if(5 == Opcode)
+		{
+			if(0 != Parameter1[0])
+            {
+                ProgramCounter= Parameter2[0];
+            }
+            else
+            {
+                /* Do Nothing */
+                ProgramCounter+=3;
+            }
+		}
+        else if(6 == Opcode)
+		{
+			if(0 == Parameter1[0])
+            {
+                ProgramCounter= Parameter2[0];
+            }
+            else
+            {
+                /* Do Nothing */
+                ProgramCounter+=3;
+            }
+		}
+        else if(7 == Opcode)
+		{
+			if(Parameter1[0] < Parameter2[0])
+            {
+                Parameter3[0]=1;
+            }
+            else
+            {
+                Parameter3[0]=0;
+            }
+            ProgramCounter+=4;
+		}
+        else if(8 == Opcode)
+		{
+			if(Parameter1[0] == Parameter2[0])
+            {
+                Parameter3[0]=1;
+            }
+            else
+            {
+                Parameter3[0]=0;
+            }
+            ProgramCounter+=4;
+		}
         else
         {
             printf("SHIT HAPPENS2!!!!!\n");
         }
+        //printf("Next Program Counter is: %d!!!!!\n", ProgramCounter);
     }
 }
 
